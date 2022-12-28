bc6e7f837db6639b97c97e5a910c8cf9820dbdf1
Marco Antonio Aguayo <MikoGrenore@Outlook.com>
2022-12-28 09:25:18 -0600

    Save ej 6

--- Ejercicio6/Animal.cs
+++ Ejercicio6/Animal.cs
@@ -8,5 +8,9 @@ namespace Ejercicio6
 {
     internal class Animal
     {
+        public string Tipo { get; set; }
+        public string ColorPelo { get; set; }  
+        public bool EsDomestico { get; set; }
+        public int NumeroPatas { get; set; }    
     }
 }
